480afabce8338e5850caace9b8b75fd95e702203
herlarney <sradecoration@gmail.com>
2022-12-21 20:55:47 +0100

    initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,12 @@
-
-
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:stacked_services/stacked_services.dart';
-
 
 void main() {
   runApp(ScreenUtilInit(
       designSize: const Size(390, 844),
       minTextAdapt: true,
-      builder: (context, child) =>  MyApp()));
+      builder: (context, child) => MyApp()));
 }
 
 class MyApp extends StatelessWidget {
@@ -19,14 +16,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Gbale',
-     navigatorKey: StackedService.navigatorKey,
-     onGenerateRoute: StackedRouter().onGenerate,
-     
+      navigatorKey: StackedService.navigatorKey,
+      onGenerateRoute: StackedRouter().onGenerate,
     );
   }
 }
-
-
-
-
-
