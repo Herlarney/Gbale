ba5d4c3bfde22bf6aab2c4d9ba98d471cc23c587
Developer Lezzycan <waheedolalekan23@gmail.com>
2022-12-28 17:55:56 +0100

    modified main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:gbale/core/theme/gbale_theme_manager.dart';
 import 'package:stacked_services/stacked_services.dart';
 
-void main() {
-  runApp(ScreenUtilInit(
-      designSize: const Size(390, 844),
-      minTextAdapt: true,
-      builder: (context, child) => const MyApp()));
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized;
+
+  SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual,
+      overlays: [SystemUiOverlay.top]).then(
+    (_) => runApp(
+      ScreenUtilInit(
+          designSize: const Size(390, 844),
+          minTextAdapt: true,
+          builder: (context, child) => const MyApp()),
+    ),
+  );
+  SystemChrome.setPreferredOrientations(
+      [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
 }
 
 class MyApp extends StatelessWidget {
@@ -17,7 +28,10 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Gbale',
       navigatorKey: StackedService.navigatorKey,
-      onGenerateRoute: StackedRouter().onGenerate,
+      //   onGenerateRoute: StackedRouter().onGenerate,
+      debugShowCheckedModeBanner: false,
+      themeMode: ThemeMode.light,
+      theme: GbaleThemeManager.lightTheme,
     );
   }
 }
