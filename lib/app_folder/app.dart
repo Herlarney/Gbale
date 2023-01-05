6b21d7547d592f59ef12990fbd4be5339d02eff4
Developer Lezzycan <waheedolalekan23@gmail.com>
2023-01-05 03:18:10 +0100

    added assest images to gbale assets

--- lib/app_folder/app.dart
+++ lib/app_folder/app.dart
@@ -1,7 +1,12 @@
+import 'package:gbale/views/manage_portfolio_view.dart';
 import 'package:stacked/stacked_annotations.dart';
+import 'package:stacked_services/stacked_services.dart';
 
 @StackedApp(
-  routes: [],
+  routes: [
+    AdaptiveRoute(page: ManagePortfolio),
+  ],
+  dependencies:[ LazySingleton(classType: NavigationService)]
 )
 class AppSetup {
   /// This serves no purpose rather than using the [StackedApp] notation.
--- lib/core/constants/gbale_assets.dart
+++ lib/core/constants/gbale_assets.dart
@@ -1,5 +1,7 @@
 class GbaleAssets {
   GbaleAssets._();
 
-  
+  static const String appBarLeadingImage = 'assets/png/gbalelogo.png';
+  static const String appBarbell = 'assets/png/bell.png';
+  static const String manageProperty = 'assets/png/manageproperty.png';
 }
